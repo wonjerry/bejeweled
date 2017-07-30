67912f476d1068e0b315cbe0acdff38d22b90011
wonjae <dldnjswo19@gmail.com>
2017-07-30 17:04:46 +0900

    I don't know where put the gameover logic

--- bejeweledLogic.js
+++ bejeweledLogic.js
@@ -25,6 +25,7 @@ BejeweledGame.prototype.init = function() {
     this.jewels.push(p5Object.loadImage("image/"+ i +".png"));
   }
 
+  /*
   for (var i = 0; i < BOARD_HEIGHT; i++) {
     var row = [];
     for (var j = 0; j < BOARD_WIDTH; j++) {
@@ -33,6 +34,18 @@ BejeweledGame.prototype.init = function() {
     }
     this.gameBoard.push(row);
   }
+  */
+
+  this.gameBoard = [
+    [1,2,1,2,1,2,1,2],
+    [3,4,3,4,3,4,3,4],
+    [1,2,1,2,1,2,1,2],
+    [3,4,3,4,3,4,3,4],
+    [1,2,1,2,1,2,1,2],
+    [3,4,3,4,3,4,3,4],
+    [1,2,1,2,1,2,1,2],
+    [3,4,3,4,3,4,3,4]
+  ];
   this.check_board();
 }
 
@@ -88,7 +101,6 @@ BejeweledGame.prototype.check_board = function() {
     this.clean_board();
     this.score += scores.length;
   }
-
   return result.length;
 }
 
@@ -120,7 +132,40 @@ BejeweledGame.prototype.run = function() {
   this.init();
 };
 
-// 이 방식은 다인용을 구현할 경우 p5 객체를 만드는 방식으로 바뀔 것이다.
+BejeweledGame.prototype.checkGameover = function(){
+  // 먼저 가로를 체크한다.
+  // 일단 나 다음번째가 같은 종류의 보석인지, 그렇다면 나의 대각선과 다음번째의 대각선에 같은 종류의 보석이 하나라도 있는지.
+  // 만약 아니라면 나의 다음 다음번째가 같은 종류의 보석인지, 그렇다면 나의 왼쪽위 왼쪽 아래가 같은 종류의 보석인지 체크하기.
+
+  // 체크시 범위가 넘어가지 않도록 잘 조절해야 한다.
+  var leftDxDy = [[-1,-1],[-1,1]];
+
+  var rightDxDy = [[2,-1],[2,1]];
+
+  for(var y = 0; y < BOARD_HEIGHT; y++){
+    for(var x = 0; x < BOARD_WIDTH - 1; x++){
+      var jewel = this.gameboard[y][x];
+
+      if( jewel === this.gameboard[y][x+1]){
+        for(var i=0; i<2; i++){
+          if(y + leftDxDy[i][1] < 0 || y + leftDxDy[i][1] >= BOARD_HEIGHT || x + leftDxDy[i][0] < 0 || x + leftDxDy[i][0] >= BOARD_WIDTH) continue;
+          if(this.gameboard[y + leftDxDy[i][1]][x + leftDxDy[i][0]] === jewel ) return false;
+          if(y + rightDxDy[i][1] < 0 || y + rightDxDy[i][1] >= BOARD_HEIGHT || x + rightDxDy[i][0] < 0 || x + rightDxDy[i][0] >= BOARD_WIDTH) continue;
+          if(this.gameboard[y + rightDxDy[i][1]][x + rightDxDy[i][0]] === jewel ) return false;
+        }
+      }else if(jewel === this.gameboard[y][x+2]){
+        for(var i=0; i<2; i++){
+          if(y + rightDxDy[i][1] < 0 || y + rightDxDy[i][1] >= BOARD_HEIGHT || x + rightDxDy[i][0] < 0 || x + rightDxDy[i][0] >= BOARD_WIDTH) continue;
+          if(this.gameboard[y + rightDxDy[i][1]][x + rightDxDy[i][0]] === jewel ) return false;
+        }
+      }
+
+
+    }
+  }
+
+  return true;
+}
 
 function draw_bejeweledGame() {
   // swap이 되어야 하는지 체크 해야 함
@@ -129,10 +174,11 @@ function draw_bejeweledGame() {
 
   // swapping?
   if (game.swap.length) {
-    var swap1_y = game.swap[0][0],
-      swap1_x = game.swap[0][1];
-    var swap2_y = game.swap[1][0],
-      swap2_x = game.swap[1][1];
+    var swap1_y = game.swap[0][0];
+    var swap1_x = game.swap[0][1];
+
+    var swap2_y = game.swap[1][0];
+    var swap2_x = game.swap[1][1];
 
     if ((game.swap_counter > 2 && !game.clean_swap) || ((game.swap_counter > 1 && game.clean_swap))) {
       var tmp = game.gameBoard[swap1_y][swap1_x];
@@ -143,7 +189,7 @@ function draw_bejeweledGame() {
       game.swap = [];
       game.swap_counter = 0;
 
-
+      //일단 위에서 사용자가 선택한 블록을 swap 해 봤는데 check 했더니 점수가 안나오는 상황이면 다시 되돌려 놓는다.
       if (!game.clean_swap) {
         var score = game.check_board();
         if (!score) {
@@ -162,7 +208,7 @@ function draw_bejeweledGame() {
     for (var x = 0; x < 8; x++) {
       var sprite_id = game.gameBoard[y][x];
 
-      if (sprite_id == -1) {
+      if (sprite_id === -1) {
         continue;
       }
 
@@ -173,7 +219,7 @@ function draw_bejeweledGame() {
 
       // selected?
       // 일단 보류
-      if (game.selected[0] == y && game.selected[1] == x) {
+      if (game.selected[0] === y && game.selected[1] === x) {
         //game.context.fillStyle = "lightgrey";
         //game.context.fillRect(x * 32, y * 32, 32, 32);
       }
@@ -184,14 +230,18 @@ function draw_bejeweledGame() {
         var speed = swap_count * 16;
       }
 
-      if (swap1_y == y && swap1_x == x) {
+      if (swap1_y === y && swap1_x === x) {
         draw_x += (swap2_x - swap1_x) * speed;
         draw_y += (swap2_y - swap1_y) * speed;
       }
 
-      if (swap2_y == y && swap2_x == x) {
+      if (swap2_y === y && swap2_x === x) {
         draw_x += (swap1_x - swap2_x) * speed;
         draw_y += (swap1_y - swap2_y) * speed;
+      }else{
+        if(game.checkGameover()){
+            game.isGameover = true;
+        }
       }
 
       p5Object.image(jewel, draw_x, draw_y);
@@ -199,7 +249,7 @@ function draw_bejeweledGame() {
   }
 
   draw_score(game.score,0,0);
-  draw_state(false,0,0);
+  draw_state(game.isGameover,0,0);
 }
 
 function draw_score(score,Sx,Sy){
@@ -237,6 +287,7 @@ var p5sketch = function(p) {
     p.draw = function(){
       p.clear();
       draw_bejeweledGame();
+      console.log(game.gameBoard);
     }
     p.mouseClicked = function(){
       // p5의 좌표 받아오기 방식 사용
